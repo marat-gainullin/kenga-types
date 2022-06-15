fa27aaa0b3914f71db45343e625ff5e0a4cc8e0e
Marat Gainullin <m.gainullin@PTCGPS.local>
2022-06-15 15:06:09 +0200

    Improvements

--- kenga-containers/tabbed-pane.d.ts
+++ kenga-containers/tabbed-pane.d.ts
@@ -4,5 +4,7 @@ declare module 'kenga-containers/tabbed-pane' {
   import Cards from 'kenga-containers/card-pane'
 
   export default class TabbedPane extends Cards {
+    captions: HTMLElement
+    captionsShell: HTMLElement
   }
 }
